a68ff42de7112284e06d3fc1e209268169bb7398
feefs <feefs@berkeley.edu>
2023-09-09 16:01:17 -0700

    feat(nvim): remove comment leader

--- dot_config/nvim/lua/options.lua
+++ dot_config/nvim/lua/options.lua
@@ -3,3 +3,7 @@ vim.o.cursorline = true
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.scrolloff = 10
+
+vim.api.nvim_create_autocmd(
+  {"FileType"}, { command = "set formatoptions-=cro" }
+)
