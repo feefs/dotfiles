3e505594b074af72b782a6e66685faa9d7fb4d1e
feefs <feefs@berkeley.edu>
2023-09-19 22:16:26 -0700

    feat(nvim): enable bi-directional search for leap

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -17,3 +17,7 @@ require("lazy").setup({
 })
 
 require("leap").add_default_mappings()
+vim.keymap.set("n", "s", function ()
+  local current_window = vim.fn.win_getid()
+  require('leap').leap { target_windows = { current_window } }
+end)
