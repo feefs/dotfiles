c285d41442db190d69a02e63b85bcfc440fb575d
feefs <feefs@berkeley.edu>
2023-09-19 18:28:07 -0700

    feat(nvim): add leap plugin

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -11,6 +11,9 @@ if not vim.loop.fs_stat(lazypath) then
 end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
-  { "catppuccin/nvim", name = "catppuccin", priority = 1000 }
-)
+require("lazy").setup({
+  { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+  { "ggandor/leap.nvim", name = "leap" }
+})
+
+require("leap").add_default_mappings()
