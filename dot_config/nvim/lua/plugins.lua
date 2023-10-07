2aac0041ee454f6244d6f0b464659127ba12e7f7
feefs <feefs@berkeley.edu>
2023-10-06 21:13:25 -0700

    fix(nvim): leap backdrop color after vscode-neovim redesigned highlighting

--- dot_config/nvim/lua/plugins.lua
+++ dot_config/nvim/lua/plugins.lua
@@ -21,4 +21,4 @@ vim.keymap.set("n", "s", function ()
   local current_window = vim.fn.win_getid()
   require('leap').leap { target_windows = { current_window } }
 end)
-vim.api.nvim_set_hl(0, 'LeapBackdrop', { link = 'Comment' })
+vim.api.nvim_set_hl(0, 'LeapBackdrop', { fg = '#777777' })
