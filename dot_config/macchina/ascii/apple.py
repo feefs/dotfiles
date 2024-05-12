ee3b9e0fd0518b380d20620465a77cecd30ff150
feefs <feefs@berkeley.edu>
2024-05-12 15:36:28 -0700

    feat(macchina): update macchina for palette spacing

--- dot_config/macchina/ascii/apple.py
+++ dot_config/macchina/ascii/apple.py
@@ -1,8 +1,8 @@
-GREEN = "\033[0;32m"
-YELLOW = "\033[0;33m"
-RED = "\033[0;31m"
-MAGENTA = "\033[0;35m"
-BLUE = "\033[0;34m"
+GREEN = "\033[32m"
+YELLOW = "\033[33m"
+RED = "\033[31m"
+MAGENTA = "\033[35m"
+BLUE = "\033[34m"
 RESET = "\033[0m"
 lines = []
 
